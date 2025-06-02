e51d9997bb6e05fd24d98ae9f7e255f792563c6c
Max Natanaelsson <max.natanaelsson@outlook.com>
2025-06-02 18:44:25 +0200

    .

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./assets/contexts/AuthContext";
+import { StrictMode } from "react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
