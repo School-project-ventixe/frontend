9beda7299e213441a03c2b8fbfd1e7faa70d432e
Max Natanaelsson <max.natanaelsson@outlook.com>
2025-06-02 22:17:14 +0200

    .

--- src/Services/Auth..js
+++ src/Services/Auth..js
@@ -2,6 +2,9 @@ import api from "./Api";
 
 export async function login(email, password) {
   const response = await api.post("/auth/login", { email, password });
+
+  console.log("login() response.data =", response.data);
+
   const token = response.data.accessToken;
   if (!token) {
     throw new Error("Ingen accessToken returnerades från servern");
